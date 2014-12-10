7069b90cfd64cfa31d2364c445ea9efdbbe26cca
Thomas Kemp <tk1g11@soton.ac.uk>
2014-12-10 11:47:59 +0000

    Commented TinyImage

--- src/main/java/uk/ac/soton/ecs/run1/TinyImage.java
+++ src/main/java/uk/ac/soton/ecs/run1/TinyImage.java
@@ -33,103 +33,116 @@ import org.openimaj.util.pair.IntFloatPair;
 
 import uk.ac.soton.ecs.Run;
 
+
 public class TinyImage implements Run {
 	
+	//Number of nearest neighbours to considers
 	private static final int K = 5;
+	//Dimension of tiny image
 	private static final int SQUARE_SIZE = 16;
 	
 	private FloatNearestNeighboursExact knn;
-	private List<String> classes;
-	private List<float[]> featureVectors;
 	
-	public static void main(String[] args){
-		try {
-			VFSGroupDataset<FImage> dataset = new VFSGroupDataset<FImage>("/Users/Tom/Desktop/training", ImageUtilities.FIMAGE_READER);
-					
-			int nTraining = 10;
-			int nTesting = 10;
-
-			GroupedRandomSplitter<String, FImage> splits =  new GroupedRandomSplitter<String, FImage>(dataset, nTraining, 0, nTesting);
-			GroupedDataset<String, ListDataset<FImage>, FImage> training = splits.getTrainingDataset();
-			GroupedDataset<String, ListDataset<FImage>, FImage> testing = splits.getTestDataset();
-			
-			TinyImage ti = new TinyImage();
-			ti.train(training);
-			
-			FImage image = testing.getRandomInstance();
-			String result = ti.classify(image);
-			System.out.println("Guess = "+result);
-			DisplayUtilities.display(image);
-			
-		} catch (FileSystemException e) {
-			e.printStackTrace();
-		}
-	}
-
-
+	//The training feature vectors
+	private List<float[]> featureVectors;
+	//The classes of the training feature vectors (array indices correspond to featureVector indices)
+	private List<String> classes;
+	
+	
+	/*
+	 * Train classifier
+	 */
 	public void train(GroupedDataset<String, ListDataset<FImage>, FImage> trainingSet) {
 		
 		classes = new ArrayList<String>();
 		featureVectors = new ArrayList<float[]>();
 		
+		//For each image in each class
 		for(String group : trainingSet.getGroups()){
 			for(FImage image : trainingSet.get(group)){
 				VectorExtractor ve = new VectorExtractor();
+				//Extract feature vector
 				float[] featureVector = ve.extractFeature(image).values;
+				
+				//Add feature vector and class to database
 				featureVectors.add(featureVector);
 				classes.add(group);
 			}
 		}
 		
+		//Array of all feature vectors
 		float[][] vectors = featureVectors.toArray(new float[featureVectors.size()][]);
+		
 		zeroMean(vectors);
 		
+		//New knn with training feature vectors
 		knn = new FloatNearestNeighboursExact(vectors);
 	}
 	
 	private void zeroMean(float[][] data){
-		
+		//TODO: zero mean & unit length
 	}
 
+	/*
+	 * Return guess of image class
+	 */
 	public String classify(FImage image) {
+		
+		//Extract feature vector for image
 		VectorExtractor ve = new VectorExtractor();
 		float[] featureVector = ve.extractFeature(image).values;
+		
+		//Find k nearest neighbours
 		List<IntFloatPair> neighbours = knn.searchKNN(featureVector, K);
+		
+		//Map classes to the number of neighbours that have that class
 		Map<String,Integer> classCount = new HashMap<String,Integer>();
+		
+		//For all neighbours
 		for(IntFloatPair result: neighbours){
+			//Get neighbour class
 			String resultClass = classes.get(result.first);
 			
 			int newCount = 1;
+			//Retrieve existing count for the class
 			if(classCount.containsKey(resultClass)){
 				newCount += classCount.get(resultClass);
 			}
+			
+			//Add 1 to class count
 			classCount.put(resultClass, newCount);
 		}
 		
+		//List of class and their count
 		List<Map.Entry<String, Integer>> guessList = new ArrayList<Map.Entry<String, Integer>>(classCount.entrySet());
+		
+		//Sort list with greatest count first
 		Collections.sort(guessList, new Comparator<Map.Entry<String, Integer>>(){
 			public int compare(Entry<String, Integer> o1, Entry<String, Integer> o2){
 				return o2.getValue().compareTo(o1.getValue());
 			}
 		});
 		
-		for(Map.Entry<String, Integer> guess : guessList){
-			System.out.println(guess.getKey() + " - "+guess.getValue());
-		}
-		
+		//Guessed class if first in list
 		String guessedClass = guessList.get(0).getKey();
 		
 		return guessedClass;
 	}
 	
+	/* 
+	 * Extract TinyImage feature vector from image
+	 */
 	class VectorExtractor implements FeatureExtractor<FloatFV,FImage>{
 
 		public FloatFV extractFeature(FImage image) {
+			//Smallest dimension of image is the size of the square
 			int size = Math.min(image.width, image.height);
+			//Extract the square from centre
 			FImage center = image.extractCenter(size, size);
+			//Resize image to tiny image
 			FImage small = center.process(new ResizeProcessor(SQUARE_SIZE, SQUARE_SIZE));
+			//2D array to 1D vector
 			float[] vector = ArrayUtils.reshape(small.pixels);
-			//TODO: zero means
 			return new FloatFV(vector);
 		}
 		
